6136b21f9acbf4692c1cbd56eb026f3a2f6d96ed
edgaras.komarovicius <edgaras.komarovicius@decodo.com>
2026-06-23 07:36:01 +0300

    Update keymap.c

--- keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
@@ -26,7 +26,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOD_G LT(7, KC_G)
 #define MOD_V LT(SYM, KC_V)
 #define SYM     10
-// #define SYM_CMD LT(SYM, KC_LGUI)
 
 // Right-hand home row mods
 #define HOME_J RCTL_T(KC_J)
@@ -126,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // layer 10 symbols (hold left CMD to activate, tap left CMD for one-shot GUI)
 [SYM] = LAYOUT(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-    _______, KC_GRV,  KC_LT,   KC_GT,   KC_MINS, KC_PIPE, KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR,  _______, _______, _______, _______,
+    _______, KC_GRV,  KC_LT,   KC_GT,   KC_MINS, KC_PIPE, KC_CIRC, KC_LCBR, KC_RCBR, KC_DLR,  KC_UNDS, _______, _______, _______,
     _______, KC_EXLM, KC_ASTR, KC_SLSH, KC_EQL,  KC_AMPR, KC_HASH, KC_LPRN, KC_RPRN, KC_SCLN, KC_DQUO, _______,          _______,
     _______, KC_TILD, KC_PLUS, KC_LBRC, KC_RBRC, KC_PERC, KC_AT,   KC_COLN, KC_COMM, KC_DOT,  KC_QUOT, _______, _______, _______,
     _______, _______, _______,                    _______,                    _______, _______, _______, _______, _______),
