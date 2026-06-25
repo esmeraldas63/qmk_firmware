77c5d6f9e7861609dd9b191e2947aee286072d86
esmeraldas63 <ekomarovicius@gmail.com>
2026-06-25 20:00:11 +0300

    Update keymap.c

--- keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
@@ -233,13 +233,6 @@ bool is_flow_tap_key(uint16_t keycode) {
     return false;
 }
 
-bool get_chordal_hold(uint16_t tap_hold_keycode, keyrecord_t *tap_hold_record, uint16_t other_keycode, keyrecord_t *other_record) {
-    if (IS_QK_MOD_TAP(tap_hold_keycode) && IS_LAYER_ON(SYM)) {
-        return false;
-    }
-    return get_chordal_hold_default(tap_hold_record, other_record);
-}
-
 char chordal_hold_handedness(keypos_t key) {
     if (key.row == MATRIX_ROWS - 1) {
         return '*';
