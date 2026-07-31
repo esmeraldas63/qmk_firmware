11a72002ee61bdb0f2bcf36b0e5a117d00d36c4d
edgaras.komarovicius <edgaras.komarovicius@decodo.com>
2026-07-31 14:56:01 +0300

    always set little flow tap

--- keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
@@ -317,7 +317,7 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_
         }
     }
 
-    return 0; // Disable Flow Tap otherwise.
+    return 30; // Disable Flow Tap otherwise.
 }
 
 char chordal_hold_handedness(keypos_t key) {
