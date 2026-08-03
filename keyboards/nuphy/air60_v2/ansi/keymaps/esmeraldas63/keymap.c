7474f38b7f4baea87995516eb4d702c9df09f1f8
edgaras.komarovicius <edgaras.komarovicius@decodo.com>
2026-08-03 15:29:00 +0300

    Update keymap.c

--- keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
+++ keyboards/nuphy/air60_v2/ansi/keymaps/esmeraldas63/keymap.c
@@ -314,7 +314,7 @@ uint16_t get_flow_tap_term(uint16_t keycode, keyrecord_t *record, uint16_t prev_
         }
     }
 
-    return 30; // Disable Flow Tap otherwise.
+    return 0; // Disable Flow Tap otherwise.
 }
 
 char chordal_hold_handedness(keypos_t key) {
